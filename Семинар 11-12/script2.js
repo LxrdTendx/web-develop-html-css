a32b048966d97b86ab4d0a88a50a38e36a05dc84
Юрий Данилов <75733634+LxrdTendx@users.noreply.github.com>
2021-11-27 12:35:09 +0300

    Update script2.js

--- Семинар 11-12/script2.js
+++ Семинар 11-12/script2.js
@@ -45,8 +45,8 @@ function getObj() {return this}
 
 city1.getCity = getObj;
 city2.getCity = getObj;
-console.log(city1.getCity().getName())
-console.log(city2.getCity().getName())
+console.log(city1.getCity().getName() + 1)
+console.log(city2.getCity().getName() + 2)
 
 //Задание 6
 
@@ -62,3 +62,65 @@ let obj = {
     }
 }
 console.log(obj.method1().method2().method3())
+
+
+//Задание 15
+function Card(from, to){
+    this.from = from;
+    this.to = to;
+}
+
+//Задание 16
+class Human {
+    #name;
+    #age;
+    #height;
+ 
+    constructor(name, age, height) {
+        this.#name = name;
+        this.#age = age;
+        this.#height = height;
+    }
+    getInfo() {
+        return `${this.#name}, ${this.#age}, ${this.#height}`;
+    }
+    get firstname() {
+        return this.#name;
+    }
+}
+ 
+let humans = [
+    new Human("Коля", 23, 180),
+    new Human("Даша", 19, 170),
+    new Human("Ваня", 18, 192),
+    new Human("Петя", 45, 178),
+    new Human("Вася", 34, 197),
+    new Human("Джони", 40, 168),
+    new Human("Катя", 37, 160),
+    new Human("Петя", 29, 200),
+    new Human("Соня", 21, 172),
+    new Human("Женя", 25, 175)
+];
+for (let human of humans)
+    console.log(human.getInfo());
+
+//Задание 17
+
+var items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic' },
+    { name: 'Zeros', value: 37 }
+];
+items.sort(function (a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    // a должно быть равным b
+    return 0;
+});
